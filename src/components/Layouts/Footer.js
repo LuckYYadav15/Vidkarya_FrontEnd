e7ddc741bea4178fc7eae3dfee8d16cf980258dd
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-02-26 11:22:25 +0530

    web counter css changed

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container-fluid">
         {/* <!-- hitwebcounter Code START --> */}
         <a href="https://www.hitwebcounter.com" target="_blank">
-          <img src="https://hitwebcounter.com/counter/counter.php?page=7951910&style=0024&nbdigits=5&type=ip&initCount=0" title="Free Counter" alt="web counter" border="0" display="none"/></a>
+          <img src="https://hitwebcounter.com/counter/counter.php?page=7951910&style=0024&nbdigits=5&type=ip&initCount=0" title="Free Counter" alt="web counter" border="0"/></a>
         <i className="social-icon fas fa-envelope"></i>
         <p>© Copyright 2022 Vidkarya</p>
       </div>
