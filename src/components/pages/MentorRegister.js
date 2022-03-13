f963e2780cf9425b1efc1052f3314f8450c31922
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-03-13 22:43:30 +0530

    multiple mentor reg

--- src/components/pages/MentorRegister.js
+++ src/components/pages/MentorRegister.js
@@ -1,27 +1,27 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "./mentorRegister.css";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 import Header from "../Layouts/Header";
-import { Link, Navigate , useNavigate} from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 
 
 export default function MentorRegister() {
 
   const { search } = useLocation();
-  const {id} = queryString.parse(search);
+  const { id } = queryString.parse(search);
   const navigate = useNavigate();
-  useEffect(async() => {
+  useEffect(async () => {
     try {
-      
-      if(!localStorage.getItem('userInfo')){
-          
+
+      if (!localStorage.getItem('userInfo')) {
+
         navigate('/auth')
       }
-      
+
     } catch (error) {
       navigate('/auth')
     }
@@ -50,45 +50,51 @@ export default function MentorRegister() {
         mentorId,
         mentorNumber,
       });
-
+      // function temp
       if (res) {
         toast.success("mentor registered successfully!!");
-      } else  {
+        const timer = setTimeout(() => {
+          navigate("/mentors");
+        }, 2500);
+      } else {
         toast.error("error in registering mentor");
       }
     } catch (error) {
       console.log(error.message);
-      toast.error("some error occured during registration!!");
+      toast.error("Mentor Already Exists !!");
+      const timer = setTimeout(() => {
+        navigate("/mentors");
+      }, 3000);
     }
   };
 
   return (
     <>
-    <Header/>
-    <section className="main-container"> 
-    <div className="mentor-container" id="mentor-form">
-        <div className="title"><h1>Mentor contact form</h1></div>
-        <div className="field">
+      <Header />
+      <section className="main-container">
+        <div className="mentor-container" id="mentor-form">
+          <div className="title"><h1>Mentor contact form</h1></div>
+          <div className="field">
             <input type="text" id="name" name="name" value={mentorData.name} onChange={handleInputChange} maxlength="32" placeholder=" " required />
             <span>Username</span>
           </div>
-        <div className="field">
+          <div className="field">
             <input type="email" id="email" name="email" value={mentorData.email} onChange={handleInputChange} required placeholder=" " pattern="\S+.*" />
             <span>User Email</span>
           </div>
-        <div className="field">
+          <div className="field">
             <input type="text" id='mentor' name="mentorId" value={id} maxlength="32" placeholder=" " required />
             <span>Mentor id</span>
           </div>
-        <div className="field">
+          <div className="field">
             <input type="text" minlength="10" name="mentorNumber" value={mentorData.mentorNumber} onChange={handleInputChange} maxlength="10" placeholder=" " required />
             <span>user Mobile no</span>
           </div>
-        <div className="button-form">
-          <button  id="btn-mentor" onClick={postMentorData}>Submit</button>
+          <div className="button-form">
+            <button id="btn-mentor" onClick={postMentorData}>Submit</button>
+          </div>
         </div>
-    </div>
-</section>
+      </section>
 
       <ToastContainer position="top-center" />
     </>
