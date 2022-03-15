a6daef638311fcc9832a21e2b2ff5e5f37d353a1
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-03-15 22:36:01 +0530

    team member added

--- src/components/API/team.js
+++ src/components/API/team.js
@@ -2,119 +2,127 @@
 const team = [
     {
         team_id: "Frontend Team",
-        details:[
-    {
-        dev_id: "01",
-        dev_name: "Daulat Jha",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/daulat56",
-        linkden:"https://www.linkedin.com/in/daulatjha/",
-        image:"images/Daulat.jpeg"
+        details: [
+            {
+                dev_id: "01",
+                dev_name: "Daulat Jha",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/daulat56",
+                linkden: "https://www.linkedin.com/in/daulatjha/",
+                image: "images/Daulat.jpeg"
 
-    },
-    {
-        dev_id: "02",
-        dev_name: "Harshit Mishra",
-        Achivement1: "ECE, 2nd Year.",
-        github: "https://github.com/HARSHITMISHRA655",
-        linkden:"https://www.linkedin.com/in/harshit-mishra-4b530b20b/",
-        image:"images/Harshit.jpg"
-    },
-    {
-        dev_id: "03", 
-        dev_name: "Hemant Dhawale",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/hemant935",
-        linkden:"https://www.linkedin.com/in/hemant-dhawale-iiit-dharwad-10966820a/",
-        image:"images/Hemant.jpeg"
-    },
-    {
-        dev_id: "04",
-        dev_name: "Keshav Jha",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/keshavjha018",
-        linkden:"https://www.linkedin.com/in/keshavjha018/",
-        image:"images/Keshav.jpeg"
-    },
-    {
-        dev_id: "05",
-        dev_name: "Ganji Nithin",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/urstrulynithin",
-        linkden:"https://www.linkedin.com/in/ganji-nithin-384940223/",
-        image:"images/Nithin.jpeg"
-    },
-    {
-        dev_id: "06",
-        dev_name: "Rajesh",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/RajeshNaidu1",
-        linkden:"https://www.linkedin.com/in/rajesh-naidu-maddila-376aa1202/",
-        image:"images/Rajesh.jpeg"
-    },
-    {
-        dev_id: "07",
-        dev_name: "Vivek Yadav",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/VivekYadav105",
-        linkden:"https://www.linkedin.com/in/vivek-yadav-58357b203",
-        image:"images/Vivek.jpeg"
-    }
+            },
+            {
+                dev_id: "02",
+                dev_name: "Harshit Mishra",
+                Achivement1: "ECE, 2nd Year.",
+                github: "https://github.com/HARSHITMISHRA655",
+                linkden: "https://www.linkedin.com/in/harshit-mishra-4b530b20b/",
+                image: "images/Harshit.jpg"
+            },
+            {
+                dev_id: "03",
+                dev_name: "Hitarth Vyas",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/Hitarth-Vyas",
+                linkden: "https://www.linkedin.com/in/",
+                image: "images/Hitarth.jpeg"
+            },
+            {
+                dev_id: "04",
+                dev_name: "Hemant Dhawale",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/hemant935",
+                linkden: "https://www.linkedin.com/in/hemant-dhawale-iiit-dharwad-10966820a/",
+                image: "images/Hemant.jpeg"
+            },
+            {
+                dev_id: "05",
+                dev_name: "Keshav Jha",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/keshavjha018",
+                linkden: "https://www.linkedin.com/in/keshavjha018/",
+                image: "images/Keshav.jpeg"
+            },
+            {
+                dev_id: "06",
+                dev_name: "Ganji Nithin",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/urstrulynithin",
+                linkden: "https://www.linkedin.com/in/ganji-nithin-384940223/",
+                image: "images/Nithin.jpeg"
+            },
+            {
+                dev_id: "07",
+                dev_name: "Rajesh",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/RajeshNaidu1",
+                linkden: "https://www.linkedin.com/in/rajesh-naidu-maddila-376aa1202/",
+                image: "images/Rajesh.jpeg"
+            },
+            {
+                dev_id: "08",
+                dev_name: "Vivek Yadav",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/VivekYadav105",
+                linkden: "https://www.linkedin.com/in/vivek-yadav-58357b203",
+                image: "images/Vivek.jpeg"
+            }
         ]
     },
     {
         team_id: "Backend Team",
-        details:[
-    {
-        dev_id: "01",
-        dev_name: "Brij Vaghani",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/Brij2003",
-        linkden:"https://www.linkedin.com/in/brij-vaghani-73aa74213/",
-        image:"images/image-default-dp.jpg"
-    },
-    {
-        dev_id: "02",
-        dev_name: "Chaitnya Giri",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/chaitnya23",
-        linkden: "https://www.linkedin.com/in/chaitnya-giri-87b180228/",
-        image:"images/Chaitnya.jpg"
-    },
-    {
-        dev_id: "03",
-        dev_name: "Hitarth Vyas",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/Hitarth-Vyas",
-        linkden:"https://www.linkedin.com/in/",
-        image:"images/Hitarth.jpeg"
-    },
-    {
-        dev_id: "04",
-        dev_name: "Neeraj",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/Kali-Decoder",
-        linkden:"https://www.linkedin.com/in/neeraj-choubisa-a4952b202/",
-        image:"images/Neeraj.jpeg"
-    },
-    {
-        dev_id: "05",
-        dev_name: "Prateek Agawral",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/prateekin",
-        linkden:"https://www.linkedin.com/in/",
-        image:"images/prateek.jpg"
-    },
-    {
-        dev_id: "06",
-        dev_name: "Tarun Sharma",
-        Achivement1: "CSE, 2nd Year.",
-        github: "https://github.com/Tarunvats9068",
-        linkden:"https://www.linkedin.com/in/tarun-sharma-4a2763207/",
-        image:"images/Tarun.jpeg"
-    }
+        details: [
+            {
+                dev_id: "01",
+                dev_name: "Brij Vaghani",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/Brij2003",
+                linkden: "https://www.linkedin.com/in/brij-vaghani-73aa74213/",
+                image: "images/image-default-dp.jpg"
+            },
+            {
+                dev_id: "02",
+                dev_name: "Chaitnya Giri",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/chaitnya23",
+                linkden: "https://www.linkedin.com/in/chaitnya-giri-87b180228/",
+                image: "images/Chaitnya.jpg"
+            },
+            {
+                dev_id: "03",
+                dev_name: "Harsh Rawat",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/harshrawat20",
+                linkden: "https://www.linkedin.com/in/",
+                image: "images/Harsh.jpeg"
+            },
+            {
+                dev_id: "04",
+                dev_name: "Neeraj",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/Kali-Decoder",
+                linkden: "https://www.linkedin.com/in/neeraj-choubisa-a4952b202/",
+                image: "images/Neeraj.jpeg"
+            },
+            {
+                dev_id: "05",
+                dev_name: "Prateek Agawral",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/prateekin",
+                linkden: "https://www.linkedin.com/in/",
+                image: "images/prateek.jpg"
+            },
+            {
+                dev_id: "06",
+                dev_name: "Tarun Sharma",
+                Achivement1: "CSE, 2nd Year.",
+                github: "https://github.com/Tarunvats9068",
+                linkden: "https://www.linkedin.com/in/tarun-sharma-4a2763207/",
+                image: "images/Tarun.jpeg"
+            }
         ]
     }
 ]
 
-export default  team ;
+export default team;
