8e0c10fe058609594d59a75eafb20d5b6bb33e58
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-04-13 11:01:06 +0530

    maths notes updated

--- src/components/API/sample_notes.js
+++ src/components/API/sample_notes.js
@@ -41,77 +41,77 @@ const samplepdfs = [
     sem: "4",
     CourseName: "Linear Algebra",
     notes: [
-      {
-        NotesId: "01_CSE_2020_IIITDWD_LA",
-        Showtag: "Author 1",
-        fileLink:
-          "https://drive.google.com/file/d/1LA8A10tI9ZCViw3ZmwpJTCVtOmueHRDe/preview",
-        DateOfUpload: "21-02-2022",
-      },
-      {
-        NotesId: "02_CSE_2020_IIITDWD_MATHS",
-        Showtag: "Author 2",
-        fileLink:
-          "https://drive.google.com/file/d/1iKUdeRaUXifZ7q9f9IH6OntlXLp4yEwz/preview",
-        DateOfUpload: "21-02-2022",
-      },
-      {
-        Showtag: "Author 3",
-        NotesId: "03_CSE_2020_IIITDWD_LA",
-        fileLink:
-          "https://drive.google.com/file/d/1Kapt_oWh9MmYwbBi3ggLuBKmQ_67uO7c/preview",
-        DateOfUpload: "21-02-2022",
-      },
+      // {
+      //   NotesId: "01_CSE_2020_IIITDWD_LA",
+      //   Showtag: "Author 1",
+      //   fileLink:
+      //     "https://drive.google.com/file/d/1LA8A10tI9ZCViw3ZmwpJTCVtOmueHRDe/preview",
+      //   DateOfUpload: "21-02-2022",
+      // },
+      // {
+      //   NotesId: "02_CSE_2020_IIITDWD_MATHS",
+      //   Showtag: "Author 2",
+      //   fileLink:
+      //     "https://drive.google.com/file/d/1iKUdeRaUXifZ7q9f9IH6OntlXLp4yEwz/preview",
+      //   DateOfUpload: "21-02-2022",
+      // },
+      // {
+      //   Showtag: "Author 3",
+      //   NotesId: "03_CSE_2020_IIITDWD_LA",
+      //   fileLink:
+      //     "https://drive.google.com/file/d/1Kapt_oWh9MmYwbBi3ggLuBKmQ_67uO7c/preview",
+      //   DateOfUpload: "21-02-2022",
+      // },
       {
         Showtag: "Author 4",
         NotesId: "04_CSE_2020_IIITDWD_LA",
-        fileLink: "https://drive.google.com/file/d/1ZoMpOloyhUW7HLJaLB5JX123xnCgvwn0/preview",
-        DateOfUpload: "16-03-2022"
+        fileLink: "https://drive.google.com/file/d/1fwUNzSUDSx0FRUA2BUceqSE1iwzpFrcQ/view?usp=sharing",
+        DateOfUpload: "13-04-2022"
       },
-      {
-        Showtag: "Author 1",
-        NotesId: "01_CSE_2020_IIITDWD_LA(21-22 March)",
-        fileLink: "https://drive.google.com/file/d/1_RwCaA_CfkboFiOzLgCxJw6u61cov6Ag/preview",
-        DateOfUpload: "26-03-2022"
-      },
-      {
-        Showtag: "Author 2",
-        NotesId: "02_CSE_2020_IIITDWD_LA(after mids)",
-        fileLink: "https://drive.google.com/file/d/16rEn_xLwyMrF4sUkEY95X8iyZDJdN_1W/preview",
-        DateOfUpload: "26-03-2022"
-      },
-      {
-        Showtag: "Author 4",
-        NotesId: "04_CSE_2020_IIITDWD_LA(10-latest March)",
-        fileLink: "https://drive.google.com/file/d/1ORhJY3b1A10Wqud47lrBpDEGgE9Ki4UY/preview",
-        DateOfUpload: "26-03-2022"
-      },
-      {
-        Showtag: "Author 7",
-        NotesId: "07_CSE_2020_IIITDWD_LA(10-23 March)",
-        fileLink: "https://drive.google.com/file/d/1l4qtFZM6oQLFDu6jYAzSsFCh93xnaXeG/preview",
-        DateOfUpload: "26-03-2022"
-      },
-    ],
-  },
-  {
-    id: "03",
-    branch: "CSE",
-    sem: "4",
-    CourseName: "Operating System",
-    notes: [
-      {
-        NotesId: "05_CSE_2020_IIITDWD_OS",
-        Showtag: "Author 5",
-        fileLink: "https://drive.google.com/file/d/1PhE5HHIMjOQygupl8oX1aZYTk6TTPSs6/preview",
-        DateOfUpload: "16-03-2022"
-      },
-      {
-        NotesId: "02_CSE_2020_IIITDWD_OS(after mids)",
-        Showtag: "Author 2",
-        fileLink: "https://drive.google.com/file/d/1FG0GUHvOZA3IxPxxZ7153MkeeQrEA_UU/preview",
-        DateOfUpload: "26-03-2022"
-      }
+  //     {
+  //       Showtag: "Author 1",
+  //       NotesId: "01_CSE_2020_IIITDWD_LA(21-22 March)",
+  //       fileLink: "https://drive.google.com/file/d/1_RwCaA_CfkboFiOzLgCxJw6u61cov6Ag/preview",
+  //       DateOfUpload: "26-03-2022"
+  //     },
+  //     {
+  //       Showtag: "Author 2",
+  //       NotesId: "02_CSE_2020_IIITDWD_LA(after mids)",
+  //       fileLink: "https://drive.google.com/file/d/16rEn_xLwyMrF4sUkEY95X8iyZDJdN_1W/preview",
+  //       DateOfUpload: "26-03-2022"
+  //     },
+  //     {
+  //       Showtag: "Author 4",
+  //       NotesId: "04_CSE_2020_IIITDWD_LA(10-latest March)",
+  //       fileLink: "https://drive.google.com/file/d/1ORhJY3b1A10Wqud47lrBpDEGgE9Ki4UY/preview",
+  //       DateOfUpload: "26-03-2022"
+  //     },
+  //     {
+  //       Showtag: "Author 7",
+  //       NotesId: "07_CSE_2020_IIITDWD_LA(10-23 March)",
+  //       fileLink: "https://drive.google.com/file/d/1l4qtFZM6oQLFDu6jYAzSsFCh93xnaXeG/preview",
+  //       DateOfUpload: "26-03-2022"
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: "03",
+  //   branch: "CSE",
+  //   sem: "4",
+  //   CourseName: "Operating System",
+  //   notes: [
+  //     {
+  //       NotesId: "05_CSE_2020_IIITDWD_OS",
+  //       Showtag: "Author 5",
+  //       fileLink: "https://drive.google.com/file/d/1PhE5HHIMjOQygupl8oX1aZYTk6TTPSs6/preview",
+  //       DateOfUpload: "16-03-2022"
+  //     },
+  //     {
+  //       NotesId: "02_CSE_2020_IIITDWD_OS(after mids)",
+  //       Showtag: "Author 2",
+  //       fileLink: "https://drive.google.com/file/d/1FG0GUHvOZA3IxPxxZ7153MkeeQrEA_UU/preview",
+  //       DateOfUpload: "26-03-2022"
+  //     }
     ],
   },
   {
