1e04c276b10033507523afd207f7b96f5413e852
chaitnya giri <88148782+chaitnya23@users.noreply.github.com>
2022-06-26 16:24:20 +0530

    Add files via upload

---
+++ src/components/Layouts/Loader.js
@@ -0,0 +1,150 @@
+import "./Loader.css";
+
+function Loader() {
+  return (
+    <>
+    
+      <section className="loading-wrapper">
+        <div className="book">
+          <div className="pages">
+            <div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+            </div>
+          </div>
+          <div className="pages">
+            <div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+            </div>
+          </div>
+          <div className="pages">
+            <div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+            </div>
+          </div>
+          <div className="pages">
+            <div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+            </div>
+          </div>
+          <div className="pages">
+            <div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+            </div>
+          </div>
+          <div className="pages">
+            <div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+            </div>
+          </div>
+          <div className="pages">
+            <div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+            </div>
+          </div>
+          <div className="pages">
+            <div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+            </div>
+          </div>
+          <div className="pages">
+            <div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+              <div className="lines"></div>
+            </div>
+          </div>
+          {/* <div className="pages">
+          <div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          </div>
+          </div>
+          <div className="pages">
+          <div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          </div>
+          </div>
+          <div className="pages">
+          <div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          </div>
+          </div>
+          <div className="pages">
+          <div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+            </div>
+            </div> -->
+            <!-- <div className="pages">
+            <div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+            </div>
+            </div>
+        <div className="pages">
+        <div>
+        <div className="lines"></div>
+        <div className="lines"></div>
+        <div className="lines"></div>
+        </div>
+        </div>
+        <div className="pages">
+        <div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+            </div>
+            </div>
+            <div className="pages">
+            <div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+            <div className="lines"></div>
+            </div>
+            </div>
+            <div className="pages">
+          <div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          </div>
+          </div>
+          <div className="pages">
+          <div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          <div className="lines"></div>
+          </div>
+        </div> */}
+          <div className="cover-page"></div>
+          <div className="cover-page-two"></div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Loader;
